dbd6a91fd0539af178346b34c241a23a8079eb47
Jean-François Poilpret <jfpoilpret@gmail.com>
2014-10-25 09:32:26 +0200

    Split PDF in 2 versions
    Split slides in 2 versions:
    - the short version, as presented during softshake
    - the long version, planned for 2x45' session

--- Arduino-ABC/sketches/AbcExp03_2FlexionSensor/AbcExp03_2FlexionSensor.ino
+++ Arduino-ABC/sketches/AbcExp03_2FlexionSensor/AbcExp03_2FlexionSensor.ino
@@ -1,3 +1,6 @@
+const int ANALOG_VALUE_0 = 133;
+const int ANALOG_VALUE_90 = 85;
+
 void setup() {
   Serial.begin(9600);
 }
@@ -5,27 +8,13 @@ void setup() {
 void loop() {
   // Get voltage on A0 analog input
   unsigned int value = analogRead(A0);
-  double Vin = Vout * value / 1024.0;
-  // compute flex sensor resistance
-  double Rflex = R2 * (Vin / Vout - 1.0);
-  // convert flex resistance
-  // consider resistance varies linearly according to flexion angle
-  // 25000  -> 0°
-  // 125000 -> 90°
-  int flex = map(Rflex, 25000, 125000, 0, 90);
+  int flex = map(value, ANALOG_VALUE_0, ANALOG_VALUE_90, 0, 90);
   
   // Display all values for debugging
   Serial.print("A0=");
   Serial.println(value);
-  Serial.print("Vin=");
-  Serial.print(Vin);
-  Serial.println("V");
-  Serial.print("Rflex=");
-  Serial.print(Rflex);
-  Serial.println("Ohm");
   Serial.print("flex=");
-  Serial.print(flex);
-  Serial.println("°");
+  Serial.println(flex);
 
   delay(500);
 }
